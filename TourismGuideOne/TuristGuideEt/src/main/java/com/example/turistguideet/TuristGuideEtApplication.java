ce8be0e7957ff439ec55f4f7f7386805c7581df8
Simon B.C <simon.bech.christensen.00@gmail.com>
2026-01-27 15:55:49 +0100

    Add files via upload

---
+++ TourismGuideOne/TuristGuideEt/src/main/java/com/example/turistguideet/TuristGuideEtApplication.java
@@ -0,0 +1,13 @@
+package com.example.turistguideet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristGuideEtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristGuideEtApplication.class, args);
+    }
+
+}
